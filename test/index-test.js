d404a97af7c0186160fddf295f89b8dc156dfc29
Mia Elisenberg <mi-ya@live.no>
2017-10-20 15:22:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -33,9 +33,9 @@ function makeInt(a.toString()) {
   parseInt('a', 10)
 }
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
+function makeInt('0x2328') {
+  0 *= 2328
+}
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
